978c055da7769e424444f43ec1d7c2e37839b521
SurovGnom <eremeevroma3@gmail.com>
2025-03-02 13:43:36 +0200

    third commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,29 @@
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../nekologo.png";
 
 
 
 function Footer() {
     return (
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top" style={{backgroundColor:"#212529",color:"#79D7BE"}}>
-                <p className="col-md-4 mb-0 ">© 2024 Company, Inc</p>
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
+                style={{backgroundColor: "#212529", color: "#79D7BE"}}>
+            <img src={logo} style={{width: "96px", height: "96px"}} alt="logo"/>
+            <p className="col-md-4 mb-0 ">© 2024 Company, Inc</p>
 
-                <a href="/"
-                   className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                </a>
+            <a href="/"
+               className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
 
-                <ul className="nav col-md-4 justify-content-end" style={{color:"#79D7BE"}}>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 ">Home</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 ">Features</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 ">Pricing</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 ">FAQs</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 ">About</a></li>
-                </ul>
-            </footer>
+            </a>
+
+            <ul className="nav col-md-4 justify-content-end" style={{color: "#79D7BE"}}>
+                <li className="nav-item"><a href="#" className="nav-link px-2 ">Home</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 ">Features</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 ">Pricing</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 ">FAQs</a></li>
+                <li className="nav-item"><a href="#" className="nav-link px-2 ">About</a></li>
+            </ul>
+        </footer>
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Dropdown from "./Dropdown";
+import logo from "../nekologo.png"
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg " style={{backgroundColor: "#212529"}}>
             <div className="container-fluid">
-                <a className="navbar" href="#">LOGO </a>
+                <a className="navbar" href="#">
+                    <img src={logo} alt="logo"/>
+                    Nekopedia
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
                         aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
